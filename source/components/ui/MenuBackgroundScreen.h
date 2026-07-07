581caf82e81a92020b3914ec83c1523d97c6e789
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2026-07-07 20:02:12 +0100

    feat: add class definition of MenuBackgroundScreen

---
+++ source/components/ui/MenuBackgroundScreen.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <nds.h>
+
+#include "components/ui/UIScreen.h"
+#include "controllers/GraphicsController.h"
+#include "core/structs.h"
+
+class MenuBackgroundScreen : public UIScreen
+{
+};
