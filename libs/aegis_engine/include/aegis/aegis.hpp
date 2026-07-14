b945f95fd6f4f12499e4ef5c1cba01bd86d559a1
Taha Rashid <taharashid710192@gmail.com>
2026-07-13 21:07:58 -0400

    Added aegis_engine as a submodule

--- libs/aegis_engine/include/aegis/aegis.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <aegis/component.hpp>
-#include <aegis/engine.hpp>
-#include <aegis/entity.hpp>
-#include <aegis/manager.hpp>
-#include <aegis/system.hpp>
-#include <aegis/types.hpp>
