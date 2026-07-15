1e8fe49bb4d783b3e0e8d9ac20c32741d0365afe
ggmini <minigsm14@gmail.com>
2026-07-15 02:36:30 +0200

    feat: added textcontrollersub class

---
+++ source/controllers/text/TextControllerSub.cpp
@@ -0,0 +1,361 @@
+#include "TextControllerSub.h"
+
+#include "core/globals.h"
+#include <fstream>
+#include <sstream>
+
+//TODO: Make this more dynamic? i.e. let us choose video mode, vram bank, ...
+
+TextControllerSub::TextControllerSub(const std::string& fontFilePath, int bgID)
+    : fontBitmap(nullptr), fontBitmapWidth(0), fontBitmapHeight(0), fontLineHeight(0), fontPalette(nullptr), bgID(bgID)
+{
+    std::string fullPath = fatBasePath + "fonts/" + fontFilePath;
+
+    //bgSetScale(bgID, 1 << 8, 1 << 8);
+    videoBuffer = (uint16_t*)bgGetGfxPtr(bgID);
+    bgSetPriority(bgID, 0);
+
+    if (!loadFontBitmap(fullPath + ".img.bin"))
+        haltOnError("Failed to load font bitmap from \n" + fullPath + ".img.bin");
+
+    if (!loadFontPalette(fullPath + ".pal.bin"))
+        haltOnError("Failed to load font palette from \n" + fullPath + ".pal.bin");
+
+    // Load metadata to learn bitmap dimensions
+    if (!loadFontMetadata(fullPath + ".fnt"))
+        haltOnError("Failed to load font metadata from \n" + fullPath + ".fnt");
+}
+
+TextControllerSub::~TextControllerSub()
+{
+    //bgSetPriority(bgID, 3); //TODO: why dos this crash the game...on the second call
+    bgSetScroll(bgID, 0, 0);
+    bgSetPriority(0, 0);
+
+    dmaFillHalfWords(0, videoBuffer, 256 * 256 * sizeof(uint16_t));
+    free(fontBitmap);
+    free(fontPalette);
+    //free(fontGlyphs);
+
+    bgHide(bgID);
+    videoBgDisable(bgID);
+}
+
+// Loader Functions =====================================================
+
+void* TextControllerSub::openFile(const std::string& path)
+{
+    FILE* file = fopen(path.c_str(), "rb");
+    if (!file)
+    {
+        return nullptr;
+    }
+
+    //get file size
+    fseek(file, 0, SEEK_END);
+    u32 size = ftell(file);
+    //return to beginning of file
+    rewind(file);
+
+    if (size == 0)
+    {
+        fclose(file);
+        return nullptr;
+    }
+
+    void* buffer = malloc(size);
+    if (buffer)
+    {
+        fread(buffer, 1, size, file);
+    }
+    fclose(file);
+
+    return buffer;
+}
+
+void* TextControllerSub::openFile(const std::string& path, u32& size)
+{
+    FILE* file = fopen(path.c_str(), "rb");
+    if (!file)
+    {
+        return nullptr;
+    }
+
+    //get file size
+    fseek(file, 0, SEEK_END);
+    size = ftell(file);
+    //return to beginning of file
+    rewind(file);
+
+    if (size == 0)
+    {
+        fclose(file);
+        return nullptr;
+    }
+
+    void* buffer = malloc(size);
+    if (buffer)
+    {
+        fread(buffer, 1, size, file);
+    }
+    fclose(file);
+
+    return buffer;
+}
+
+bool TextControllerSub::loadFontBitmap(const std::string& path)
+{
+    void* buffer = openFile(path);
+    if (buffer == nullptr)
+    {
+        return false;
+    }
+
+    fontBitmap = reinterpret_cast<std::uint8_t*>(buffer);
+
+    return true;
+}
+
+bool TextControllerSub::loadFontPalette(const std::string& path)
+{
+    void* buffer = openFile(path);
+    if (buffer == nullptr)
+    {
+        return false;
+    }
+
+    fontPalette = reinterpret_cast<std::uint16_t*>(buffer);
+    dmaCopy(fontPalette, BG_PALETTE_SUB, 256 * sizeof(uint16_t)); // Copy the palette to the background palette memory
+    return true;
+}
+
+bool TextControllerSub::loadFontMetadata(const std::string& path)
+{
+    u32 size;
+    void* buffer = openFile(path, size);
+    if (buffer == nullptr)
+    {
+        return false;
+    }
+
+    // create a string from the in-memory buffer and read it line-by-line
+    std::string content(reinterpret_cast<char*>(buffer), size);
+    free(buffer);
+
+    std::istringstream iss(content);
+    std::string line;
+    while (std::getline(iss, line))
+    {
+        //Get the common data
+        if (line.rfind("common ", 0) == 0)
+        {
+            fontLineHeight = extractIntValue(line, "lineHeight=");
+            fontBitmapWidth = extractIntValue(line, "scaleW=");
+            fontBitmapHeight = extractIntValue(line, "scaleH=");
+            continue;
+        }
+
+        //If theres no character in this line move to the next
+        if (line.rfind("char ", 0) != 0)
+        {
+            continue;
+        }
+
+        const int charId = extractIntValue(line, "id=");
+        if (charId < 0 || charId >= 256)
+        {
+            continue;
+        }
+
+        //Create Glyph for the current char
+        Glyph glyph{};
+        glyph.xPos = extractIntValue(line, "x=");
+        glyph.yPos = extractIntValue(line, "y=");
+        glyph.width = extractIntValue(line, "width=");
+        glyph.height = extractIntValue(line, "height=");
+        glyph.xOffset = extractIntValue(line, "xoffset=");
+        glyph.yOffset = extractIntValue(line, "yoffset=");
+        fontGlyphs[charId] = glyph;
+    }
+
+    //Fallback values in case the font metadata is missing
+    if (fontLineHeight <= 0)
+    {
+        fontLineHeight = 32;
+    }
+
+    if (fontBitmapWidth <= 0)
+    {
+        fontBitmapWidth = 256;
+    }
+
+    if (fontBitmapHeight <= 0)
+    {
+        fontBitmapHeight = 256;
+    }
+
+    //TODO: check if glyphs were loaded correctly?
+
+    return true;
+}
+
+// Actual Drawing Functions =================================================
+
+void TextControllerSub::drawText(const std::string& text, int startX, int startY, int color)
+{
+    int cursorX = startX;
+    int cursorY = startY;
+
+    for (char c : text)
+    {
+        //Handle Newline
+        if (c == '\n')
+        {
+            cursorX = startX;
+            cursorY += fontLineHeight + 2;
+            continue;
+        }
+
+        Glyph g = fontGlyphs[static_cast<unsigned char>(c)];
+
+        //Handle automatic word wrapping to prevent drawing outside the screen bounds
+        //TODO: Look ahead for whole words (fix check word wrap)
+        if (cursorX + g.width > 256)
+        {
+            cursorX = startX;
+            cursorY += fontLineHeight + 2;
+        }
+
+        for (int y = 0; y < g.height; y++)
+        {
+            for (int x = 0; x < g.width; x++)
+            {
+                int srcX = g.xPos + x;
+                int srcY = g.yPos + y;
+                int pixelIndex = srcY * fontBitmapWidth + srcX;
+
+                sassert(pixelIndex / 2 < fontBitmapWidth * fontBitmapHeight / 2,
+                        "Pixel index out of bounds for font bitmap");
+
+                // TODO: check which fontlineheight is the minimum where we need to start doing this
+                //int pixelValue = fontLineHeight > 16 ? extractPixelValue(pixelIndex) : fontBitmap[pixelIndex];
+
+                int pixelValue = fontBitmap[pixelIndex];
+
+                //If pixel is not black, draw it to screen (we're using 2 to try to filter out pixels that would be too dark to be seen)
+                if (pixelValue > 0)
+                {
+                    int screenX = cursorX + x;
+                    int screenY = cursorY + g.yOffset + y;
+                    //Only draw if within screen bounds otherwise disregard (from my experience this shouldn't crash the game, but text wrapping around would still be a bit of a problem)
+                    if (screenX >= 0 && screenX < 256 && screenY >= 0 && screenY < 192)
+                        drawPixel(screenX, screenY, pixelValue);
+                }
+            }
+        }
+        cursorX += g.width + 1;
+    }
+}
+
+void TextControllerSub::clearScreen()
+{
+    dmaFillHalfWords(0, videoBuffer, 256 * 256 * sizeof(uint16_t)); // Clear the video buffer
+}
+
+// Helper Functions ======================================================
+
+void TextControllerSub::drawPixel(int x, int y, int paletteIndex)
+{
+    int wordIndex = (y * 256 + x) / 2;
+    u16 currentWord = videoBuffer[wordIndex];
+    if (x % 2 == 0)
+    {
+        // Clear the lower 8 bits, then inject our 8-bit color index
+        videoBuffer[wordIndex] = (currentWord & 0xFF00) | (fontPalette[paletteIndex] & 0xFF);
+    }
+    else
+    {
+        // Clear the upper 8 bits, then inject our 8-bit color index shifted up
+        videoBuffer[wordIndex] = (currentWord & 0x00FF) | ((fontPalette[paletteIndex] & 0xFF) << 8);
+    }
+}
+
+int TextControllerSub::extractIntValue(const std::string& line, const std::string& key)
+{
+    const std::size_t keyPos = line.find(key);
+    if (keyPos == std::string::npos) //position not found
+    {
+        return 0;
+    }
+
+    const std::size_t dataStart = keyPos + key.size();
+    std::size_t dataEnd = dataStart;
+    while (dataEnd < line.size() && line[dataEnd] != ' ')
+    {
+        dataEnd++;
+    }
+
+    return std::stoi(line.substr(dataStart, dataEnd - dataStart));
+}
+
+int TextControllerSub::extractPixelValue(const int pixelIndex)
+{
+    // Divide index by 2 to target the correct byte in memory
+    std::uint8_t rawByte = fontBitmap[pixelIndex / 2];
+
+    // Extract the proper 4-bit color index depending on pixel parity
+    if (pixelIndex % 2 == 0)
+    {
+        return rawByte & 0x0F; // First pixel is lower 4 bits
+    }
+    else
+    {
+        return (rawByte >> 4) & 0x0F; // Second pixel is upper 4 bits
+    }
+}
+
+//TODO: FIX: THIS IS CURRENTLY BROKEN
+bool TextControllerSub::checkWordWrap(const std::string& word, int startX)
+{
+    int cursorX = startX;
+
+    for (char c : word)
+    {
+        Glyph g = fontGlyphs[static_cast<unsigned char>(c)];
+        cursorX += g.width + g.xOffset;
+    }
+    if (cursorX > 256)
+    {
+        return true; // Word exceeds screen width
+    }
+    return false; // Word fits within screen width
+}
+
+void TextControllerSub::haltOnError(const std::string& errorMessage)
+{
+    consoleDemoInit();
+    iprintf("Error: %s\n", errorMessage.c_str());
+    while (1)
+        swiWaitForVBlank();
+}
+
+// Test Functions ========================================================
+
+void TextControllerSub::testBitmap()
+{
+    for (int y = 0; y < 256; y++)
+    {
+        for (int x = 0; x < 256; x++)
+        {
+            videoBuffer[y * 256 + x] = fontBitmap[y * fontBitmapWidth + x] | BIT(15);
+        }
+    }
+}
+
+void TextControllerSub::testPalette()
+{
+    for (int i = 0; i < (256 * 256); i++)
+    {
+        videoBuffer[i] = fontPalette[i % 256] | BIT(15);
+    }
+}
---
+++ source/controllers/text/TextControllerSub.h
@@ -0,0 +1,118 @@
+/**
+ * @file TextControllerSub.h
+ * @brief Controller for rendering text from a bitmap on the Nintendo DS.
+ * @author ggmini
+ */
+
+#include "core/structs.h"
+#include "nds.h"
+#include <cstdint>
+#include <string>
+
+/**
+ * @brief A class that handles rendering text from a bitmap on the Nintendo DS.
+ *
+ * This class provides functionality to load a font from a bitmap and render text to the sub screen using that font.
+ */
+class TextControllerSub
+{
+  public:
+    TextControllerSub(const std::string& fontFilePath = "skip-std-b/size-8/size-8",
+                      int bgID = -1); //TODO: REMOVE DEFAULT
+    ~TextControllerSub();
+
+    /**
+     * @brief Draws the specified text at the given coordinates with the specified color.
+     *
+     * @param text The text to be drawn.
+     * @param x The x-coordinate where the text will start.
+     * @param y The y-coordinate where the text will start.
+     * @param color The color of the text in ARGB16 format. Default is white.
+     */
+    void drawText(const std::string& text, int x, int y, int color = ARGB16(1, 31, 31, 31));
+    /**
+     * @brief Clears the screen.
+     *
+     * This function resets all pixels of the background, effectively clearing any previously drawn content.
+     * Should be used when removing text from screen, i.e. to draw new content.
+     */
+    void clearScreen();
+
+    /**
+     * @brief Draws the bitmap to the screen for testing purposes.
+     *
+     * This funnction is intended for testing the bitmap.
+     * It draws the entire bitmap to the screen.
+     */
+    void testBitmap();
+    /**
+     * @brief Draws the palette to the screen for testing purposes.
+     *
+     * This function is intended for testing the palette.
+     * It draws the entire palette to the screen.
+     */
+    void testPalette();
+
+  private:
+    uint16_t* videoBuffer;
+
+    /**
+     * @brief Loads the font bitmap from a file.
+     *
+     * @param path The path to the font bitmap file.
+     * @return true if the font bitmap was loaded successfully, false otherwise.
+     * @retval true The font bitmap was loaded successfully.
+     * @retval false The font bitmap could not be loaded.
+     */
+    bool loadFontBitmap(const std::string& path);
+    /**
+     * @brief Loads the font palette from a file.
+     *
+     * @param path The path to the font palette file.
+     * @return true if the font palette was loaded successfully, false otherwise.
+     * @retval true The font palette was loaded successfully.
+     * @retval false The font palette could not be loaded.
+     */
+    bool loadFontPalette(const std::string& path);
+    /**
+     * @brief Loads the font metadata from a file.
+     *
+     * @param path The path to the font metadata file.
+     * @return true if the font metadata was loaded successfully, false otherwise.
+     * @retval true The font metadata was loaded successfully.
+     * @retval false The font metadata could not be loaded.
+     */
+    bool loadFontMetadata(const std::string& path);
+    /**
+     * @brief Opens a file and reads its contents into memory.
+     *
+     * @param path The path to the file to be opened.
+     * @return A pointer to the memory buffer containing the file's contents, or nullptr if the file could not be opened.
+     * @note The caller is responsible for freeing the memory buffer when it is no longer needed.
+     */
+    void* openFile(const std::string& path);
+    /**
+     * @brief Opens a file and reads its contents into memory.
+     *
+     * @param path The path to the file to be opened.
+     * @param size A reference to a variable that will hold the size of the file in bytes.
+     * @return A pointer to the memory buffer containing the file's contents, or nullptr if the file could not be opened.
+     * @note The caller is responsible for freeing the memory buffer when it is no longer needed.
+     */
+    void* openFile(const std::string& path, u32& size);
+
+    std::uint8_t* fontBitmap;
+    std::uint16_t* fontPalette;
+    int fontBitmapWidth;
+    int fontBitmapHeight;
+    int fontLineHeight;
+    uint16_t bgID; // Store the background ID for cleanup
+
+    Glyph fontGlyphs[256];
+
+    void drawPixel(int x, int y, int color);
+    bool checkWordWrap(const std::string& text, int startX);
+    int extractPixelValue(const int pixelIndex);
+    int extractIntValue(const std::string& line, const std::string& key);
+    void haltOnError(const std::string& errorMessage);
+};
