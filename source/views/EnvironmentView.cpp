e0ec76a8943784a2018f74db392607ca6f2012b4
Taha Rashid <taharashid710192@gmail.com>
2026-07-19 17:42:59 -0400

    Added fog, character outline support
    The character has a light, black line around the model (Metaphor-style). The fog is enabled, but is pushed back (not visible) as it was messing up the camera

--- source/views/EnvironmentView.cpp
+++ source/views/EnvironmentView.cpp
@@ -91,25 +91,54 @@ void EnvironmentView::init()
     vramSetBankI(VRAM_I_SUB_SPRITE_EXT_PALETTE);
     bgExtPaletteEnableSub();
 
-    // main screen, 3D
+    // 3D init
     glInit();
-    glEnable(GL_ANTIALIAS);
-    glEnable(GL_TEXTURE_2D); // enable texturing
+    glEnable(GL_ANTIALIAS);  // cleans up edges
+    glEnable(GL_TEXTURE_2D); // for textures
+    // glEnable(GL_BLEND);      // useful for UI
+    glEnable(GL_FOG);     // fog effect
+    glEnable(GL_OUTLINE); // stylistic outline
 
     glClearColor(0, 0, 0, 31);
     glClearPolyID(63);
     glClearDepth(0x7FFF);
 
-    // set size of main screen
+    // viewport
     glViewport(0, 0, 255, 191);
-
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     // zNear is how close the camera can see, zFar is the maximum draw distance
     gluPerspective(55, 256.0 / 192.0, 0.1, 40);
 
-    glPolyFmt(POLY_ALPHA(31) | POLY_CULL_BACK);
-    glColor3b(255, 255, 255); // keep white so texture colors aren't tinted
+    // outline
+    glSetOutlineColor(0, RGB15(0, 0, 0));
+
+    // fog
+    // setup color
+    glFogColor(22, 25, 28, 31); // daytime blue
+    // glFogColor(30, 25, 16, 31);  // evening orange
+    // glFogColor(16, 17, 19, 31);  // rainy gray
+
+    // how much depth difference there is between table entries
+    glFogShift(shift);
+    // depth at which the fog starts (and the table starts applying)
+    glFogOffset(depth);
+
+    // generate a linear density table
+    int density = 0;
+    for (int i = 0; i < 32; i++) // it has 32 steps
+    {
+        glFogDensity(i, density);
+        // exponentially increase mass the furthur back the fog is
+        density += (mass * i) >> 2;
+
+        // entries are 7 bit, so cap the density to 127
+        if (density > 127)
+            density = 127;
+    }
+
+    glPolyFmt(POLY_ALPHA(31) | POLY_CULL_BACK | POLY_FOG);
+    glColor3b(255, 255, 255);
 
     dbEntry = getEnvironmentDbEntry();
     if (!dbEntry)
@@ -343,24 +372,20 @@ ViewState EnvironmentView::update()
                   camPos.upY,
                   camPos.upZ);
 
+        // environment
         glPushMatrix();
-
+        glPolyFmt(POLY_ALPHA(31) | POLY_CULL_BACK | POLY_FOG | POLY_ID(0));
         env.draw();
-
         env.drawBillboards(Globals::enableBillboards, camPos.cameraX, camPos.cameraY, camPos.cameraZ);
-
         glPopMatrix(1);
 
+        // model
         glPushMatrix();
-
         CharacterPosition charPos = playerCtrl->isCharacterAt();
-
         glTranslatef(charPos.x, charPos.y, charPos.z);
-
         glRotatef(charPos.facingAngle, 0.0f, 1.0f, 0.0f);
-
+        glPolyFmt(POLY_ALPHA(31) | POLY_CULL_BACK | POLY_FOG | POLY_ID(1));
         characterAnimationCtrl->render();
-
         glPopMatrix(1);
 
         glFlush(0);
